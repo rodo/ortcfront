3bdf231dfb7edb133c755097e30287e4b50f79b3
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-10-03 13:45:53 +0200

    Add file for jenkins conf

---
+++ ortcfront/settings/test_settings.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014 Rodolphe Quiédeville <rodolphe@quiedeville.org>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Settings used on jenkins server
+#
+import os
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/tmp/ortcfront.sqlite',     # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+INSTALLED_APPS += (
+    'django_jenkins',
+    'django_nose',
+)
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.django_tests',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_sloccount',
+    )
+
+
+PROJECT_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+PYLINT_RCFILE = os.path.join(PROJECT_ROOT_PATH, '..', '..', 'pylint.rc')
+#
+# Minimal
+#
+USER_AGENT = "http://exmaple.com, contact webmaster@example.com"
